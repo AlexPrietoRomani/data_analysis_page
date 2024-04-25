8b89d1f3de91454dacd45d535a2ff1c96ac1507a
Alex Prieto <alexprieto1997@gmail.com>
2024-04-25 14:21:44 -0500

    Cambio de funciones
    cambio de función de los gráficos

--- main_page.py
+++ main_page.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 def leer_archivo(archivo):
     """
@@ -15,7 +16,7 @@ def leer_archivo(archivo):
         st.error("Formato de archivo no admitido. Por favor, sube un archivo Excel o CSV.")
         return None
 
-def generar_boxplot(df, x_column, y_column):
+def generar_boxplot(df, x_column, y_column, z_column):
     """
     Genera un gráfico boxplot para las columnas especificadas de un DataFrame.
 
@@ -23,19 +24,36 @@ def generar_boxplot(df, x_column, y_column):
         df: El DataFrame con los datos.
         x_column: La columna para el eje x.
         y_column: La columna para el eje y.
+        z_column: La columna para el filtro.
 
     Returns:
         Un gráfico boxplot.
     """
-    plt.figure(figsize=(10, 6))
-    sns.boxplot(data=df, x=df[x_column], y=df[y_column], palette= "hls")
-    plt.title(f"Gráfico boxplot: {y_column} vs {x_column}")
-    plt.xlabel(x_column)
-    plt.ylabel(y_column)
-    plt.grid()
+    #valor de z_columna poner como default en None
+    z_columna = None
+    
+    #Cambiar valor de z_columna si existe una selección de el 4to parametro
+    if z_columna is None:
+        z_columna = z_column
+    
+    #Creando condicial si existe valor de z_column
+    if z_columna is None:
+        plt.figure(figsize=(10, 6))
+        sns.boxplot(data=df, x=df[x_column], y=df[y_column], palette= "hls")
+        plt.title(f"Gráfico boxplot: {y_column} vs {x_column}")
+        plt.xlabel(x_column)
+        plt.ylabel(y_column)
+        plt.grid()
+    if z_column is not None:
+        plt.figure(figsize=(10, 6))
+        sns.boxplot(data=df, x=df[x_column], y=df[y_column], palette= "hls", hue= df[z_column])
+        plt.title(f"Gráfico boxplot: {y_column} vs {x_column}")
+        plt.xlabel(x_column)
+        plt.ylabel(y_column)
+        plt.grid()
     return plt
 
-def generar_lineplot(df, x_column, y_column):
+def generar_lineplot(df, x_column, y_column, z_column):
     """
     Genera un gráfico boxplot para las columnas especificadas de un DataFrame.
 
@@ -47,12 +65,56 @@ def generar_lineplot(df, x_column, y_column):
     Returns:
         Un gráfico boxplot.
     """
+    #valor de z_columna poner como default en None
+    z_columna = None
+    
+    #Cambiar valor de z_columna si existe una selección de el 4to parametro
+    if z_columna is None:
+        z_columna = z_column
+    
+    #Creando condicial si existe valor de z_column
+    if z_columna is None:
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(data=df, x=df[x_column], y=df[y_column], palette= "hls")
+        plt.title(f"Gráfico boxplot: {y_column} vs {x_column}")
+        plt.xlabel(x_column)
+        plt.ylabel(y_column)
+        plt.grid()
+    if z_columna is not None:
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(data=df, x=df[x_column], y=df[y_column], palette= "hls", hue= df[z_columna])
+        plt.title(f"Gráfico boxplot: {y_column} vs {x_column}")
+        plt.xlabel(x_column)
+        plt.ylabel(y_column)
+        plt.grid()
+    return plt
+
+def generar_boxplot_suma_acumulado(df, x_column, y_column, z_column):
+    """
+    Genera un gráfico boxplot para las columnas especificadas de un DataFrame.
+
+    Args:
+        df: El DataFrame con los datos.
+        x_column: La columna para el eje x.
+        y_column: La columna para el eje y.
+        z_column: La columna para agrupar por repetición.
+
+    Returns:
+        Un gráfico boxplot.
+    """
+    #Sumanos la agrupaciones de los Kg
+    df = df.groupby([x_column,z_column])[y_column].sum().reset_index()
+    # Convertir la columna 'Plot' a categórica
+    df[x_column] = df[x_column].astype(str)    
+    df[x_column] = pd.Categorical(df[x_column])
+    # Creamos un objeto Figure
     plt.figure(figsize=(10, 6))
-    sns.lineplot(data=df, x=df[x_column], y=df[y_column], palette= "hls")
-    plt.title(f"Gráfico boxplot: {y_column} vs {x_column}")
+    sns.boxplot(data=df, x=df[x_column], y=df[y_column], palette= "hls")
+    plt.title(f"Gráfico boxplot: {y_column} vs {x_column} Acumulate")
     plt.xlabel(x_column)
     plt.ylabel(y_column)
     plt.grid()
+    
     return plt
 
 def main():
@@ -78,14 +140,18 @@ def main():
                 # Configurar la selección de columnas para los ejes x e y
                 x_column = st.selectbox("Seleccionar columna para el eje x", columnas)
                 y_column = st.selectbox("Seleccionar columna para el eje y", columnas)
+                z_column = st.selectbox("Seleccionar columna para filtro o columna de repetición", columnas, index = None)
+                
+                
+                st.markdown(f"{z_column}")
 
-                # Display graph based on user input
                 if st.button("Generate Boxplot"):
-                    grafico = generar_boxplot(df, x_column, y_column)
+                    grafico = generar_boxplot(df, x_column, y_column, z_column)
                 # Display graph based on user input
                 if st.button("Generate Lineplot"):
-                    grafico = generar_lineplot(df, x_column, y_column)
-
+                    grafico = generar_lineplot(df, x_column, y_column, z_column)
+                if st.button("Generate Boxplot Acumulate"):
+                    grafico = generar_boxplot_suma_acumulado(df, x_column, y_column, z_column)
                 # Mostrar el gráfico
                 st.pyplot(grafico)
             else:
